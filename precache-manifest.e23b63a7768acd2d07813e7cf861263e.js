4781a37dc8c796e08092bb9e3fc55c02e6ac963c
Øistein Haugland <haugland.1193@gmail.com>
2019-02-11 20:29:44 +0100

    Updates

---
+++ precache-manifest.e23b63a7768acd2d07813e7cf861263e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce0cbf6e8c0f3c6e9d1d",
+    "url": "/oisteinhaugland/simple-chat-app/static/js/runtime~main.ce0cbf6e.js"
+  },
+  {
+    "revision": "6232160421c454e4e636",
+    "url": "/oisteinhaugland/simple-chat-app/static/js/main.62321604.chunk.js"
+  },
+  {
+    "revision": "c4c3e6c00734137a2e05",
+    "url": "/oisteinhaugland/simple-chat-app/static/js/2.c4c3e6c0.chunk.js"
+  },
+  {
+    "revision": "6232160421c454e4e636",
+    "url": "/oisteinhaugland/simple-chat-app/static/css/main.0ca0bc80.chunk.css"
+  },
+  {
+    "revision": "15769bdd7d3ef4f28b7b980090884811",
+    "url": "/oisteinhaugland/simple-chat-app/index.html"
+  }
+];
